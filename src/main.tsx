a09639aa808202e566775fb0956ae413d1401448
jose Angel <josegallardo.cor@gmail.com>
2025-10-09 00:00:07 -0600

    feat: configuracion inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HooksApp } from './HooksApp'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HooksApp />
   </StrictMode>,
 )
