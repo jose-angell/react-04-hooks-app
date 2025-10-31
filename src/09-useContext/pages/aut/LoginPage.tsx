05f23a5a4183fbb430f2d04184b70be0757973c0
jose Angel <josegallardo.cor@gmail.com>
2025-10-30 23:08:01 -0600

    feat: navegar entre pantallas

--- src/09-useContext/pages/aut/LoginPage.tsx
+++ src/09-useContext/pages/aut/LoginPage.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Link } from 'react-router'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='flex flex-col items-center min-h-screen'>
+      <h1 className='text-4xl font-bold'> Login Page</h1>
+      <hr/>
+      <form className='flex flex-col gap-2 my-10'>
+        <input type="text" placeholder='ID del usuario'
+        />
+        <Button type='submit' >Loing</Button>
+      </form>
+      <Link to="/about">
+        <Button variant="ghost">volver a la pagina principal</Button>
+      </Link>
+    </div>
   )
 }
--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 >Perfil del usuario</h1>
+      <hr/>
+      <pre className='my-4'>{JSON.stringify({}, null, 2)}</pre>
+      <Button variant="destructive">Salir</Button>
+    </div>
   )
 }
